7b97ed107d631c3e4a83a1e55100dd6ddda82dce
avan1235 <macieekprocyk@gmail.com>
2021-12-27 00:39:17 +0100

    Generate sources on Kotlin build

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,35 +1,36 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
-    kotlin("jvm") version "1.6.10"
-    antlr
+  kotlin("jvm") version "1.6.10"
+  antlr
 }
 
 group = "ml.dev.kotlin"
 version = "1.0"
 
 repositories {
-    mavenCentral()
+  mavenCentral()
 }
 
 dependencies {
-    implementation(kotlin("stdlib"))
+  implementation(kotlin("stdlib"))
 
-    implementation("org.antlr:antlr4-runtime:4.9.3")
-    antlr("org.antlr:antlr4:4.9.3")
+  implementation("org.antlr:antlr4-runtime:4.9.3")
+  antlr("org.antlr:antlr4:4.9.3")
 
-    testImplementation("org.junit.jupiter:junit-jupiter-api:5.8.2")
-    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.8.2")
+  testImplementation("org.junit.jupiter:junit-jupiter-api:5.8.2")
+  testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.8.2")
 }
 
 tasks.getByName<Test>("test") {
-    useJUnitPlatform()
+  useJUnitPlatform()
 }
 
 tasks.withType<KotlinCompile> {
-    kotlinOptions.jvmTarget = "${JavaVersion.VERSION_1_8}"
+  kotlinOptions.jvmTarget = "${JavaVersion.VERSION_1_8}"
+  dependsOn(tasks.generateGrammarSource)
 }
 
 tasks.generateGrammarSource {
-    arguments = listOf("-visitor", "-package", "ml.dev.kotlin.syntax")
+  arguments = listOf("-visitor", "-package", "ml.dev.kotlin.syntax")
 }
