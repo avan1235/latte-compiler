2abace6c2ab1df40c285ff3f8dba9ed472192a24
avan1235 <macieekprocyk@gmail.com>
2021-12-29 21:50:28 +0100

    revert latc main

--- src/main/kotlin/ml/dev/kotlin/latte/Main.kt
+++ src/main/kotlin/ml/dev/kotlin/latte/Main.kt
@@ -5,23 +5,20 @@ import ml.dev.kotlin.latte.syntax.parse
 import ml.dev.kotlin.latte.typecheck.typeCheck
 import ml.dev.kotlin.latte.util.LatteException
 import ml.dev.kotlin.latte.util.eprintln
-import ml.dev.kotlin.latte.util.invoke
-import ml.dev.kotlin.latte.util.unit
 import java.io.File
 
-fun main(args: Array<String>): Unit = "pwd"().unit()
-//  args.takeIf { it.isNotEmpty() }?.forEach { path ->
-//  try {
-//    File(path).runCompiler()
-//    println("OK")
-//  } catch (e: LatteException) {
-//    eprintln(e.userMessage)
-//    eprintln("ERROR")
-//  } catch (e: Throwable) {
-//    eprintln("Unknown error: $e")
-//    eprintln("ERROR")
-//  }
-//} ?: println("Usage: ./latte <input-file-paths>")
+fun main(args: Array<String>): Unit = args.takeIf { it.isNotEmpty() }?.forEach { path ->
+  try {
+    File(path).runCompiler()
+    println("OK")
+  } catch (e: LatteException) {
+    eprintln(e.userMessage)
+    eprintln("ERROR")
+  } catch (e: Throwable) {
+    eprintln("Unknown error: $e")
+    eprintln("ERROR")
+  }
+} ?: println("Usage: ./latte <input-file-paths>")
 
 private fun File.runCompiler() = takeIf { it.isFile }?.inputStream()
   ?.parse()?.also { println("AST:\n$it") }
--- src/main/kotlin/ml/dev/kotlin/latte/asm/Nasm.kt
+++ src/main/kotlin/ml/dev/kotlin/latte/asm/Nasm.kt
@@ -6,14 +6,14 @@ import ml.dev.kotlin.latte.util.exeFile
 import ml.dev.kotlin.latte.util.invoke
 import java.io.File
 
-fun nasm(asmFile: File) {
-  val asmDir = asmFile.dir.absolutePath
-  val name = asmFile.nameWithoutExtension
-  val oFile = File(asmDir, "$name.o")
+fun nasm(assembly: File) {
+  val asmDir = assembly.dir.absolutePath
+  val name = assembly.nameWithoutExtension
+  val o = File(asmDir, "$name.o")
   val libFile = File(exeFile().dir.absolutePath).resolve("lib").resolve("runtime.o")
   val result = File(asmDir, name)
-  "nasm -f elf32 ${asmFile.absolutePath} -o ${oFile.absolutePath}"().checkCode()
-  "gcc -m32 ${libFile.absolutePath} ${oFile.absolutePath} -o ${result.absolutePath}"().checkCode()
+  "nasm -f elf32 ${assembly.absolutePath} -o ${o.absolutePath}"().checkCode()
+  "gcc -m32 ${libFile.absolutePath} ${o.absolutePath} -o ${result.absolutePath}"().checkCode()
 }
 
 private fun Int.checkCode(): Unit = if (this != 0) throw CompileException(this) else Unit
