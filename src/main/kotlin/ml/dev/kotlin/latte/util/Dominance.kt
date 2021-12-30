c36ad4be91aedeef48844136430e57c09843e10b
avan1235 <macieekprocyk@gmail.com>
2021-12-30 22:20:46 +0100

    rename dominance

--- src/main/kotlin/ml/dev/kotlin/latte/util/Dominance.kt
+++ src/main/kotlin/ml/dev/kotlin/latte/util/Dominance.kt
@@ -18,14 +18,14 @@ class Dominance<V>(private val root: V, private val graph: Graph<V>) {
 
   private val postOderIdx: Map<V, Int> = postOrder.mapIndexed { idx, v -> v to idx }.toMap()
   private val reversePostOrder: List<V> = postOrder.reversed()
-  private val _doms: MutableMap<V, V> = HashMap()
-  private val _dfSet: MutableDefaultMap<V, HashSet<V>> = MutableDefaultMap({ HashSet() })
+  private val _dominator: MutableMap<V, V> = HashMap()
+  private val _frontiers: MutableDefaultMap<V, HashSet<V>> = MutableDefaultMap({ HashSet() })
 
   /**
    * For node v we define its dominator a node u such as u dominates v, so every path
    * from root node to v goes through u. It's called strict dominator if u != v.
    */
-  fun dominator(v: V): V = _doms[v] ?: err("Dominator not calculated for $v in $graph")
+  fun dominator(v: V): V = _dominator[v] ?: err("Dominator not calculated for $v in $graph")
 
   /**
    * For node v we define its dominance frontiers as a set of nodes, which has
@@ -33,24 +33,24 @@ class Dominance<V>(private val root: V, private val graph: Graph<V>) {
    * (so there exists some other path from root node to these nodes that don't
    * go through v)
    */
-  fun dominanceFrontiers(v: V): Set<V> = _dfSet[v]
+  fun dominanceFrontiers(v: V): Set<V> = _frontiers[v]
 
   private fun calcDominators() {
-    _doms[root] = root
+    _dominator[root] = root
     var changed = true
     while (changed) {
       changed = false
       for (b in reversePostOrder) {
         if (b == root) continue
         val predecessors = graph.predecessors(b)
-        val firstPredecessor = predecessors.first { _doms[it] != null }
+        val firstPredecessor = predecessors.first { _dominator[it] != null }
         var idom = firstPredecessor
         for (pred in predecessors) {
           if (pred == firstPredecessor) continue
-          if (_doms[pred] != null) idom = intersect(pred, idom)
+          if (_dominator[pred] != null) idom = intersect(pred, idom)
         }
-        if (_doms[b] != idom) {
-          _doms[b] = idom
+        if (_dominator[b] != idom) {
+          _dominator[b] = idom
           changed = true
         }
       }
@@ -61,8 +61,8 @@ class Dominance<V>(private val root: V, private val graph: Graph<V>) {
     var finger1 = b1
     var finger2 = b2
     while (finger1 != finger2) {
-      while (postOderIdx[finger1]!! < postOderIdx[finger2]!!) finger1 = _doms[finger1]!!
-      while (postOderIdx[finger2]!! < postOderIdx[finger1]!!) finger2 = _doms[finger2]!!
+      while (postOderIdx[finger1]!! < postOderIdx[finger2]!!) finger1 = _dominator[finger1]!!
+      while (postOderIdx[finger2]!! < postOderIdx[finger1]!!) finger2 = _dominator[finger2]!!
     }
     return finger1
   }
@@ -73,9 +73,9 @@ class Dominance<V>(private val root: V, private val graph: Graph<V>) {
       if (predecessors.size < 2) continue
       for (p in predecessors) {
         var runner = p
-        while (runner != _doms[b]) {
-          _dfSet[runner] += b
-          runner = _doms[runner]!!
+        while (runner != _dominator[b]) {
+          _frontiers[runner] += b
+          runner = _dominator[runner]!!
         }
       }
     }
