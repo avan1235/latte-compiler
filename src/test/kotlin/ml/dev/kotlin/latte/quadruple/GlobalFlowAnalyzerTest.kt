764dfc9ee01fa3e0231f90f81d6bf2f4c3e52631
avan1235 <macieekprocyk@gmail.com>
2022-01-04 16:46:03 +0100

    add while test for global test analysis

--- src/test/kotlin/ml/dev/kotlin/latte/quadruple/GlobalFlowAnalyzerTest.kt
+++ src/test/kotlin/ml/dev/kotlin/latte/quadruple/GlobalFlowAnalyzerTest.kt
@@ -20,6 +20,10 @@ internal class GlobalFlowAnalyzerTest {
         return x;
       }
     """,
+    /**
+     * 0: f@int(x#0):
+     * 1:  ret x#0
+     */
     label = "f" mangled listOf(IntType),
     aliveBefore = mapOf(
       0 to setOf(),
@@ -57,21 +61,21 @@ internal class GlobalFlowAnalyzerTest {
       }
     """,
     /**
-     * 0: 	f@int(x#0):
-     * 1: 	  y@1#0 = 0
-     * 2: 	  if x#0 eq 42 goto @L2
-     * 3: 	@G6:
-     * 4: 	  goto @L3
-     * 5: 	@L3:
-     * 6: 	  y@1#3 = x#0
-     * 7: 	  y@1#2 = y@1#3
-     * 8: 	  goto @L4
-     * 9: 	@L2:
-     * 10:	  @T5#0 = neg x#0
-     * 11:	  y@1#1 = @T5#0
-     * 12:	  y@1#2 = y@1#1
-     * 13:	@L4:
-     * 14:	  ret y@1#2
+     * 0:  f@int(x#0):
+     * 1:    y@1#0 = 0
+     * 2:    if x#0 eq 42 goto @L2
+     * 3:  @G6:
+     * 4:    goto @L3
+     * 5:  @L3:
+     * 6:    y@1#3 = x#0
+     * 7:    y@1#2 = y@1#3
+     * 8:    goto @L4
+     * 9:  @L2:
+     * 10:   @T5#0 = neg x#0
+     * 11:   y@1#1 = @T5#0
+     * 12:   y@1#2 = y@1#1
+     * 13: @L4:
+     * 14:   ret y@1#2
      */
     label = "f" mangled listOf(IntType),
     aliveBefore = mapOf(
@@ -143,15 +147,77 @@ internal class GlobalFlowAnalyzerTest {
       14 to setOf(intLoc("y@1#2")),
     ),
   )
+
+  @Test
+  fun `test while function example`() = testGlobalFlow(
+    program = """
+      int main() {
+        return f(42);
+      }
+      int f(int x) {
+        int y = 0;
+        while (y < x) {
+          y++;
+        }
+        return y;
+      }
+    """,
+    /**
+     * 0:	 f@int(x#0):
+     * 1:	   y@1#0 = 0
+     * 2:	   y@1#1 = y@1#0
+     * 3:	   goto @L3
+     * 4:	 @L2:
+     * 5:	   y@1#2 = inc y@1#1
+     * 6:	   y@1#1 = y@1#2
+     * 7:	 @L3:
+     * 8:	   if y@1#1 lt x#0 goto @L2
+     * 9:	 @G5:
+     * 10:   goto @L4
+     * 11: @L4:
+     * 12:   ret y@1#1
+     */
+    label = "f" mangled listOf(IntType),
+    aliveBefore = mapOf(
+      0 to setOf(),
+      1 to setOf(intArg("x#0", 0)),
+      2 to setOf(intArg("x#0", 0), intLoc("y@1#0")),
+      3 to setOf(intArg("x#0", 0), intLoc("y@1#1")),
+      4 to setOf(intArg("x#0", 0), intLoc("y@1#1")),
+      5 to setOf(intArg("x#0", 0), intLoc("y@1#1")),
+      6 to setOf(intArg("x#0", 0), intLoc("y@1#2")),
+      7 to setOf(intArg("x#0", 0), intLoc("y@1#1")),
+      8 to setOf(intArg("x#0", 0), intLoc("y@1#1")),
+      9 to setOf(intLoc("y@1#1")),
+      10 to setOf(intLoc("y@1#1")),
+      11 to setOf(intLoc("y@1#1")),
+      12 to setOf(intLoc("y@1#1")),
+    ),
+    aliveAfter = mapOf(
+      0 to setOf(intArg("x#0", 0)),
+      1 to setOf(intArg("x#0", 0), intLoc("y@1#0")),
+      2 to setOf(intArg("x#0", 0), intLoc("y@1#1")),
+      3 to setOf(intArg("x#0", 0), intLoc("y@1#1")),
+      4 to setOf(intArg("x#0", 0), intLoc("y@1#1")),
+      5 to setOf(intArg("x#0", 0), intLoc("y@1#2")),
+      6 to setOf(intArg("x#0", 0), intLoc("y@1#1")),
+      7 to setOf(intArg("x#0", 0), intLoc("y@1#1")),
+      8 to setOf(intLoc("y@1#1"), intArg("x#0", 0)),
+      9 to setOf(intLoc("y@1#1")),
+      10 to setOf(intLoc("y@1#1")),
+      11 to setOf(intLoc("y@1#1")),
+      12 to setOf(),
+    ),
+  )
 }
 
 private fun testGlobalFlow(
   program: String,
   label: String,
-  aliveBefore: Map<Int, Set<VirtualReg>>,
-  aliveAfter: Map<Int, Set<VirtualReg>>,
-  definedAt: Map<Int, Set<VirtualReg>>,
-  usedAt: Map<Int, Set<VirtualReg>>,
+  aliveBefore: Map<Int, Set<VirtualReg>>? = null,
+  aliveAfter: Map<Int, Set<VirtualReg>>? = null,
+  definedAt: Map<Int, Set<VirtualReg>>? = null,
+  usedAt: Map<Int, Set<VirtualReg>>? = null,
 ) {
   val (graph, _) = program.byteInputStream().parse().typeCheck().toIR()
   with(graph) {
@@ -164,10 +230,10 @@ private fun testGlobalFlow(
 
   assertEquals(cfg.orderedBlocks().sumOf { it.statements.count() }, analysis.statements.size)
 
-  analysis.statements.testAnalysis("aliveBefore", aliveBefore, analysis.aliveBefore)
-  analysis.statements.testAnalysis("aliveAfter", aliveAfter, analysis.aliveAfter)
-  analysis.statements.testAnalysis("definedAt", definedAt, analysis.definedAt)
-  analysis.statements.testAnalysis("usedAt", usedAt, analysis.usedAt)
+  aliveBefore?.let { analysis.statements.testAnalysis("aliveBefore", it, analysis.aliveBefore) }
+  aliveAfter?.let { analysis.statements.testAnalysis("aliveAfter", it, analysis.aliveAfter) }
+  definedAt?.let { analysis.statements.testAnalysis("definedAt", it, analysis.definedAt) }
+  usedAt?.let { analysis.statements.testAnalysis("usedAt", it, analysis.usedAt) }
 }
 
 private fun intLoc(reg: String): LocalValue =
