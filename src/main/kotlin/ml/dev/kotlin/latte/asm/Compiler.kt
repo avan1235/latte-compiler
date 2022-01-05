fcd375cd734e8f8ae3586f6bff338792e059b028
avan1235 <macieekprocyk@gmail.com>
2022-01-05 23:49:07 +0100

    clean tests code

--- src/main/kotlin/ml/dev/kotlin/latte/asm/Compiler.kt
+++ src/main/kotlin/ml/dev/kotlin/latte/asm/Compiler.kt
@@ -24,7 +24,6 @@ private class Compiler(
   private fun IR.compileFunctions(): String = buildString {
     graph.functions.values.forEach {
       val analysis = GlobalFlowAnalyzer.analyze(it).peepHoleOptimize()
-      println(analysis.statements.nlString { it.repr() })
       FunctionCompiler(analysis, strings, this, labelGenerator).compile()
     }
   }
@@ -40,26 +39,3 @@ private class Compiler(
     }
   }
 }
-
-private fun ValueHolder.repr(): String = when (this) {
-  is BooleanConstValue -> "$bool"
-  is IntConstValue -> "$int"
-  is StringConstValue -> label.name
-  is ArgValue -> id
-  is LocalValue -> id
-}
-
-private fun Quadruple.repr(): String = when (this) {
-  is AssignQ -> "${to.repr()} = ${from.repr()}"
-  is RelCondJumpQ -> "if ${left.repr()} ${op.name.lowercase()} ${right.repr()} goto ${toLabel.name}"
-  is BinOpQ -> "${to.repr()} = ${left.repr()} ${op.name.lowercase()} ${right.repr()}"
-  is UnOpQ -> "${to.repr()} = ${op.name.lowercase()} ${from.repr()}"
-  is FunCodeLabelQ -> "${label.name}(${args.joinToString { it.repr() }}):"
-  is CodeLabelQ -> "${label.name}:"
-  is CondJumpQ -> "if ${cond.repr()} goto ${toLabel.name}"
-  is JumpQ -> "goto ${toLabel.name}"
-  is FunCallQ -> "${to.repr()} = call ${label.name} (${args.joinToString { it.repr() }})"
-  is RetQ -> "ret${value?.let { " ${it.repr()}" } ?: ""}"
-  is UnOpModQ -> "${to.repr()} = ${op.name.lowercase()} ${from.repr()}"
-  is PhonyQ -> "${to.repr()} = phi (${from.toList().joinToString(", ") { "${it.first.name}:${it.second.repr()}" }})"
-}.let { if (this is Labeled) it else "  $it" }
